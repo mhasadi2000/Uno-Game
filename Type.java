d1fc61f51e70f2ec1de56760a617316e2a08e9b3
Mohammad Hossein Asadi <59434209+mhasadi2000@users.noreply.github.com>
2020-04-14 17:50:56 +0430

    add a method to get index of enum.

--- Type.java
+++ Type.java
@@ -2,4 +2,14 @@ package com.company;
 
 public enum Type {
     N1,N2,N3,N4,N5,N6,N7,N8,N9,N0,P2,P4,RV,BL,CC;
+
+    private static Type[] list = Type.values();
+
+    public static Type getType(int i) {
+        return list[i];
+    }
+
+    public static int listGetLastIndex() {
+        return list.length - 1;
+    }
 }
