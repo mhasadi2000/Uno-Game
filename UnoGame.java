7c3e536064dbcb07e1279e6bf27fed12b693c043
Mohammad Hossein Asadi <59434209+mhasadi2000@users.noreply.github.com>
2020-04-14 17:52:58 +0430

    create cardSet in UnoGame class.

--- UnoGame.java
+++ UnoGame.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 
 public class UnoGame {
     private ArrayList<CardSet> cardSets;
+    private Repository repository;
     public UnoGame(){
+        cardSets=new ArrayList<>();
+        repository=new Repository();
+    }
+
+    public void createCardSet(){
+        CardSet cardSet=new CardSet();
+        cardSets.add(cardSet);
+        for (int j=0;j<7;j++){
+            int rt= (int) (Math.random() * 8);
+            int rc= (int) (Math.random() * 8);
+            Card card=repository.getCard(Type.getType(rt),CColor.getColor(rc));
+            cardSet.addCard(card);
+            repository.removeCard(card);
+        }
     }
 }
