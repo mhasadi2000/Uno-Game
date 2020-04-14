adf74c3e7b0b68a9c7186dcddc480f7c12067663
Mohammad Hossein Asadi <59434209+mhasadi2000@users.noreply.github.com>
2020-04-14 17:47:46 +0430

    add card in CardSet.

--- CardSet.java
+++ CardSet.java
@@ -4,8 +4,17 @@ import java.util.ArrayList;
 
 public class CardSet {
     private ArrayList<Card> cards;
+
     public CardSet(){
         cards=new ArrayList<>();
+    }
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    public void removeCard(Card card){
+        cards.remove(card);
     }
 
     public ArrayList<Card> getCards() {
