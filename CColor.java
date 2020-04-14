80bb67fdeeb6abe8f6a5809998f33241c405a530
Mohammad Hossein Asadi <59434209+mhasadi2000@users.noreply.github.com>
2020-04-14 17:49:58 +0430

    add a method to get index of enum.

--- CColor.java
+++ CColor.java
@@ -2,4 +2,14 @@ package com.company;
 
 public enum CColor {
     RED,BLUE,GREEN,YELLOW,BLACK;
+
+    private static CColor[] list = CColor.values();
+
+    public static CColor getColor(int i) {
+        return list[i];
+    }
+
+    public static int listGetLastIndex() {
+        return list.length - 1;
+    }
 }
